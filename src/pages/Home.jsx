ab367132fc63087665eff28074e7cc20504a8678
NivyaS2k15 <148306006+NivyaS2k15@users.noreply.github.com>
2025-08-06 16:48:45 +0530

    view is completed
    view is complteted

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,34 +1,78 @@
 import React, { useEffect } from 'react'
 import Header from '../components/Header'
 import { Link } from 'react-router-dom'
-import { useDispatch } from 'react-redux'
+import { useDispatch, useSelector } from 'react-redux'
 import { fetchProducts } from '../redux/slices/productSlice'
+import loadingimages from '../assets/loadingimage.gif'
 
 
 const Home = () => {
 
-   const dispatch =  useDispatch()
-   useEffect(()=>{
-    dispatch(fetchProducts())
-   },[])
+    const dispatch = useDispatch()
+    const { allproducts, loading, errorMsg } = useSelector(state => state.ProductReducer)
+    console.log(allproducts, loading, errorMsg);
+    console.log("errormesssss", errorMsg);
+
+
+    useEffect(() => {
+        dispatch(fetchProducts())
+    }, [])
 
     return (
         <>
 
             <Header insideHome={true} />
             <div style={{ paddingTop: '100px' }} className='container px-4 mx-auto'>
-                <div className='grid grid-cols-4 gap-4'>
-                    {/* card duplicate */}
-                    <div className='rounded border p-3 shadow'>
+                {/* card duplicate */}
 
-                        <img height={'200px'} width={'100%'} src="https://m.media-amazon.com/images/I/61fJzMoAj5L.jpg" alt="image" />
-                        <div className='text-center'>
-                            <h3 className='text-xl font-bold'>Product Name</h3>
-                            <Link className='bg-violet-500 rounded p-1 mt-2 text-white inline-block' to={'/id/view'} >View More</Link>
+
+                { // if its loading
+                    loading ?
+                        <div className='flex justify-center items-center my-5 text-lg'>
+                            <img width={'700px'} height={'700px '} src={loadingimages} alt="" />
+                            loading..............
                         </div>
-                    </div>
+                        :
 
-                </div>
+                        // card
+                        <>
+                            <div className='grid grid-cols-4 gap-4'>
+                                {/*  this is where we need to duplicate cards  so javascript is writiiten*/}
+
+                                {
+
+
+                                  allproducts?.length>0?
+                                  //show product
+                                  allproducts?.map(products=>(
+                                     <div key={products?.id} className='rounded border p-3 shadow'>
+
+                                        <img height={'200px'} width={'100%'} src={products.thumbnail} alt="image" />
+                                        <div className='text-center'>
+                                            <h3 className='text-xl font-bold'>{products.title}</h3>
+                                            <Link className='bg-violet-500 rounded p-1 mt-2 text-white inline-block' to={`/${products?.id}/view`} >View More</Link>
+                                        </div>
+                                    </div>
+
+                                  ))
+
+
+                                   
+                                    : 
+
+                                    //else product not found
+
+                                    <div  className='flex justify-center items-center font-bold text-red-400 text-3xl'>
+                                        Product not found!!!!
+                                    </div>
+                                }
+
+
+                            </div>
+                        </>
+
+
+                }
 
             </div>
         </>
--- src/redux/slices/productSlice.js
+++ src/redux/slices/productSlice.js
@@ -1,42 +1,44 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
-               
+
 //action return promise 
-export const fetchProducts = createAsyncThunk("products/fetchProducts", async ()=>{
+export const fetchProducts = createAsyncThunk("products/fetchProducts", async () => {
     const result = await axios.get("https://dummyjson.com/products")
     console.log(result);
     //const resulta = result.data.products
     //console.log("resulta",resulta);
-   // return resulta
-   return result.data.products
+    // return resulta
 
-    
-    
+    sessionStorage.setItem("allproducts",JSON.stringify(result.data.products))
+    return result.data.products
+
+
+
 })
 
- const productSlice = createSlice({
-    name:"products",
-    initialState:{
-        allproducts:[],
-        loading:false,
-        errorMsg:""
+const productSlice = createSlice({
+    name: "products",
+    initialState: {
+        allproducts: [],
+        loading: false,
+        errorMsg: ""
 
     },
-    reducers:{
-        
+    reducers: {
+
     },
-    extraReducers:(builder)=>{
-        builder.addCase(fetchProducts.fulfilled,(state,apiResult)=>{
+    extraReducers: (builder) => {
+        builder.addCase(fetchProducts.fulfilled, (state, apiResult) => {
             state.allproducts = apiResult.payload
             state.loading = false
             state.errorMsg = ""
         })
-         builder.addCase(fetchProducts.pending,(state,apiResult)=>{
+        builder.addCase(fetchProducts.pending, (state, apiResult) => {
             state.allproducts = []
             state.loading = true
             state.errorMsg = ""
         })
-        builder.addCase(fetchProducts.rejected,(state,apiResult)=>{
+        builder.addCase(fetchProducts.rejected, (state, apiResult) => {
             state.allproducts = []
             state.loading = false
             state.errorMsg = "Api  call failed"
